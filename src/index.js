c7e7f8e4e957052627df3a655b98bdb193d71ece
shivagayathri <shivagayathrik2000@gmail.com>
2025-09-04 11:26:13 +0530

    added rate imiter for security

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { swaggerUi, swaggerSpec } = require("../swagger");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 app.use(express.json());
 
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+
 const authRoutes = require("./routes/auth");
+
+
+const signupLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: { error: "Too many signup attempts, please try again later." }
+});
+
+app.use("/auth/signup", signupLimiter); 
 app.use("/auth", authRoutes);
+
 
 mongoose.connect("mongodb://localhost:27017/auth-service")
   .then(() => {
-    console.log("MongoDB connected");
-    app.listen(3000, () => console.log("Server running on port 3000"));
+    console.log("✅ MongoDB connected");
+    app.listen(3000, () => console.log("🚀 Server running on port 3000"));
   })
-  .catch(err => console.error(err));
+  .catch(err => console.error("DB Connection Error:", err));
